18d0abfd9311b6013c64432cf4407e520fb1a432
BePine <BePine17@gmail.com>
2024-06-07 21:48:25 +0200

    added fields, created osntartanimation for textBlocks.

--- onStartAnimations.js
+++ onStartAnimations.js
@@ -1,5 +1,34 @@
-function startingMainFunc (){
- var element = document.getElementById("main");
+function startingMainFunc() {
+    var element = document.getElementById("main");
+    var containerElements = document.getElementsByClassName("container");
+    var thirdContainer = document.getElementById("third")
+    var thirdTextBlocks = [];
+
+    for (var i = 0; i < containerElements.length; i++) {
+        containerElements[i].classList.toggle("hidden")
+        containerElements[i].classList.toggle("shown")
+
+    }
+    var textBlocksElements = document.getElementsByClassName("textBlocks");
+    for (var i = 0; i < textBlocksElements.length; i++) {
+
+        if (thirdContainer.contains(textBlocksElements[i])) {
+            thirdTextBlocks.push(textBlocksElements[i]);
+        }
+        else {
+            textBlocksElements[i].classList.toggle("hidden")
+            textBlocksElements[i].classList.toggle("shown")
+        }
+    }
+    setTimeout(() => { applyToThirdContainer(thirdTextBlocks); }, 800)
     element.classList.add("after");
 
+}
+function applyToThirdContainer(thirdTextBlocks) {
+    for (var i = 0; i < thirdTextBlocks.length; i++)
+{
+
+    thirdTextBlocks[i].classList.toggle("hidden")
+    thirdTextBlocks[i].classList.toggle("shown")
+}
 }
